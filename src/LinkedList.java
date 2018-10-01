da45ebb80ec53c88684dbd52bb5c708b9dd5f592
League <league@WTS14-2.attlocal.net>
2018-09-30 17:45:19 -0700

    First commit

---
+++ src/LinkedList.java
@@ -0,0 +1,47 @@
+
+
+public class LinkedList<T> {
+	
+	private Node<T> first = null;
+	
+	public Node<T> getFirst() {
+		return first;
+	}
+
+	public void setFirst(Node<T> first) {
+		this.first = first;
+	}
+
+	public static <T>LinkedList<T> init(){
+		return new LinkedList<>();
+	}
+	
+	public void append(T element) {
+		Node<T> newNode = new Node<T>(element);
+		Node<T> lastNode = first;
+		
+		if (first == null) {
+			first = newNode;					
+		} else {
+			while(lastNode.getNext() != null) {
+				lastNode = lastNode.getNext();
+			}
+			lastNode.setNext(newNode);
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		String affiche = new String();
+		Node<T> parcours = first;
+		while (parcours.getClass() != null) {
+			//System.out.println(parcours.getValue());
+			affiche += parcours.getValue() + " -- ";
+			parcours.getNext();
+		}
+		return affiche;
+	}
+
+	
+}
---
+++ src/LinkedListRunner.java
@@ -0,0 +1,13 @@
+
+
+public class LinkedListRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> maListe = new LinkedList<>();
+		
+		maListe.append("test");
+		System.out.println(maListe.toString());
+	}
+
+}
